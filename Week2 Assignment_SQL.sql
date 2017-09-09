03bd74dc6d96c353ac4d5f3182fad63be1ae4b60
jameskuruvilla <jamesmsda@gmail.com>
2017-09-09 17:01:34 -0400

    Week 2 Assignment

---
+++ Week2 Assignment_SQL.sql
@@ -0,0 +1,174 @@
+DROP TABLE IF EXISTS movie_rating ;
+
+CREATE TABLE `Movie_Rating` (
+  `Movie_name` varchar(30),
+  `Rated_by`  varchar(30),
+  `Rating`   int(2) ,
+  `Creation_Date_Time` date
+) ;
+
+-- Rating For 'Home Again'
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Home Again', 'Person1',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Home Again', 'Person2',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Home Again', 'Person3',2,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Home Again', 'Person4',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Home Again', 'Person5',3,now());
+
+-- Rating for Beyond the mask
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Beyond the mask', 'Person1',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Beyond the mask', 'Person2',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Beyond the mask', 'Person3',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Beyond the mask', 'Person4',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Beyond the mask', 'Person5',3,now());
+
+
+-- Rating for Leep
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Leep', 'Person1',2,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Leep', 'Person2',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Leep', 'Person3',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Leep', 'Person4',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Leep', 'Person5',4,now());
+
+
+
+-- Rating for Dancer and the Dame
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Dancer and the Dame', 'Person1',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Dancer and the Dame', 'Person2',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Dancer and the Dame', 'Person3',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Dancer and the Dame', 'Person4',2,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Dancer and the Dame', 'Person5',3,now());
+
+
+
+-- Rating for Daddy's Home
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Daddy\'s Home', 'Person1',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Daddy\'s Home', 'Person2',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Daddy\'s Home', 'Person3',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Daddy\'s Home', 'Person4',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('Daddy\'s Home', 'Person5',4,now());
+
+-- Rating for I am not ashamed
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('I am not ashamed', 'Person1',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('I am not ashamed', 'Person2',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('I am not ashamed', 'Person3',4,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('I am not ashamed', 'Person4',3,now());
+
+INSERT INTO MOVIE_RATING
+(`Movie_name` ,   `Rated_by`  ,  `Rating`  ,  `Creation_date_time` )
+VALUES
+('I am not ashamed', 'Person5',3,now());
